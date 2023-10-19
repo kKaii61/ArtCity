6ff301d8c0bd210e620bd6bb56bddd7e494c307a
kKaii61 <21h1120041@ut.edu.vn>
2023-10-19 14:09:12 +0700

    Optimize Size

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,12 @@ function banner() {
     var i;
     var x = document.getElementsByClassName("banner-item");
     for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
+        x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(banner, 2000);    
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(banner, 2000);
 }
 
 function carousel() {
@@ -22,8 +22,7 @@ function carousel() {
     const arrowBtns = document.querySelectorAll(".slideshow-container i");
     const firstCardWidth = carousel.querySelector(".carousel-slide").offsetWidth;
     let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-    
-    
+
     arrowBtns.forEach(btn => {
      btn.addEventListener("click", () => {
       carousel.scrollLeft += btn.id === "left" ?  Math.ceil(-firstCardWidth) : Math.ceil(firstCardWidth);
