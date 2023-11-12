d3c82110576bef44f8c4449c0a5e39eb8f314bce
kKaii61 <21h1120041@ut.edu.vn>
2023-11-12 12:10:29 +0700

    search script

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,3 +2,8 @@ document.querySelector('.header__search-icon-wrap').addEventListener('click', fu
     var searchBar = document.querySelector('.header__search-input-wrap');
     searchBar.style.display = (searchBar.style.display === 'block') ? 'none' : 'block';
 });
+
+document.querySelector('.header__search-btn').addEventListener('click', function () {
+    var searchBar = document.querySelector('.header__search-input-wrap');
+    searchBar.style.display = 'none';
+});
