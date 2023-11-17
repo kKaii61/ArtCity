6f0bbe622bb0c1ccef60178903318b1e392ea146
kKaii61 <21h1120041@ut.edu.vn>
2023-11-17 12:58:01 +0700

    add confirm password

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,11 +1,32 @@
 const inputs = document.getElementsByClassName('required-input');
 const radioContainer = document.querySelector('.radio-container-gender');
 const radioInputs = radioContainer.querySelectorAll('input[type=radio]');
+const selectNation = document.querySelector("select");
 // const checkboxContainer = document.querySelector('.checkbox');
 // const boxInputs = checkboxContainer.querySelectorAll('input[type=checkbox]');
 const warningmessage = document.getElementsByClassName('warningMessage');
 const errorMessage = document.querySelectorAll('.warningMessage');
 
+const pwrd_1 = document.querySelector("#pwrd_1");
+const pwrd_2 = document.querySelector("#pwrd_2");
+const errorText = document.querySelector(".error-text");
+const signup = document.querySelector(".signup-btn");
+
+function checkPassword() {
+    if(pwrd_1.value == "" && pwrd_2.value == ""){
+
+    }else{
+        if(pwrd_1.value != pwrd_2.value ){
+            errorText.style.display = "block";
+            errorText.textContent = "Confirm Password Not Match";
+            return false;
+          }else{
+            errorText.style.display = "block";
+            errorText.textContent = "Confirm Password Matched";
+            return false;
+          }
+    }
+}
 
 function changeBackground() {
     for(var i = 0; i < inputs.length; i++){
@@ -20,7 +41,7 @@ function changeBackground() {
         }
     }
     for(var i = 0; i < radioInputs.length; i++){
-        if (radioInputs[i].checked) {
+        if (radioInputs[0].checked || radioInputs[1].checked) {
             radioContainer.style.backgroundColor = '';
             errorMessage[2].style.display = 'none';
         } else {
@@ -28,6 +49,12 @@ function changeBackground() {
             errorMessage[2].style.display = 'block';
         }
     }
+    if(selectNation.value == "") {
+        warningmessage[3].style.display = 'block';
+    } else{
+        warningmessage[3].style.display = 'none';
+    }
+
     // for(var i = 0; i < boxInputs.length; i++){
     //     if (boxInputs[i].checked) {
     //         checkboxContainer.style.backgroundColor = '';
@@ -50,7 +77,7 @@ function checkBackgroundColor() {
         }
     }
     for(var i = 0; i < radioInputs.length; i++){
-        if (radioInputs[i].checked) {
+        if (radioInputs[0].checked || radioInputs[1].checked){
             radioContainer.style.backgroundColor = '';
             errorMessage[2].style.display = 'none';
         } else {
